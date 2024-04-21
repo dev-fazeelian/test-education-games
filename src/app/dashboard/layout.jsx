fb430ed1fd6650cbeee24d3bf540b4b89c604242
dev-fazeelian <elian.ibarrac2@gmail.com>
2024-04-20 23:28:26 -0500

    Creation of layouts for nested routes using '{children}' param in a view component

---
+++ src/app/dashboard/layout.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const DashboardLayout = ({children}) => {
+  return (
+    <>
+        <h1>Dashboard Layout</h1>
+        {children}
+    </>
+  )
+}
+
+export default DashboardLayout
---
+++ src/app/games/layout.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const GamesLayout = ({children}) => {
+  return (
+    <>
+        <h1>Games Layout</h1>
+        {children}
+    </>
+  )
+}
+
+export default GamesLayout
